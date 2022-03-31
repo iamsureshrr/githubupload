97716d5868c25bea635df1f70d2a6caf267aff96
SuryaGuna99 <100912157+SuryaGuna99@users.noreply.github.com>
2022-03-31 06:25:19 +0530

    Update Github.java

--- Github.java
+++ Github.java
@@ -2,6 +2,6 @@ public class Github{
 public static void main(String[] args){
 	System.out.println("Good morning dude");
 	System.out.println("Good Afternoon bro");
-	System.out.println("Good night moms");
+	System.out.println("Good night momsffff");
 }
 }
