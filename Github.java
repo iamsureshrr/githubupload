63d74215704305449462c9f57a831abbf6fac85e
SuryaGuna99 <100912157+SuryaGuna99@users.noreply.github.com>
2022-03-31 05:54:55 +0530

    Add files via upload

---
+++ Github.java
@@ -0,0 +1,7 @@
+public class Github{
+public static void main(String[] args){
+	System.out.println("Good morning dude");
+	System.out.println("Good Afternoon bro");
+	System.out.println("Good night moms");
+}
+}
